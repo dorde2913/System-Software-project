07a815987d77f2d5cf318562adb1f52533cfec10
Djordje <dj13372913@gmail.com>
2024-05-14 01:55:35 -0700

    Odradjene sve instrukcije osim za rad sa memorijom

--- src/Assembler.cpp
+++ src/Assembler.cpp
@@ -1,5 +1,5 @@
 #include "../inc/Assembler.hpp"
-
+#include <bitset>
 void Assembler::startAssembly(std::string input_file,std::string output_file){
   this->file_name = input_file;
   this->output_name = output_file;
@@ -179,10 +179,11 @@ void Assembler::addLine(Line& line){
     }
     else if (instruction == "int"){
       //softverski prekid, nema op
-      section_contents[current_section].push_back(0);
-      section_contents[current_section].push_back(0);
-      section_contents[current_section].push_back(0);
       section_contents[current_section].push_back(16);
+      section_contents[current_section].push_back(0);
+      section_contents[current_section].push_back(0);
+      section_contents[current_section].push_back(0);
+      
 
     }
     else if (instruction == "iret"){
@@ -196,16 +197,17 @@ void Assembler::addLine(Line& line){
       //r14 JE SP, r15 JE PC
       location_counter+=4;
       //pop pc
-
-      section_contents[current_section].push_back(4);//najnizi bajt
-      section_contents[current_section].push_back(0);
-      section_contents[current_section].push_back(254);
       section_contents[current_section].push_back(147);//najvisi bajt
-
-      section_contents[current_section].push_back(4);
+      section_contents[current_section].push_back(254);
       section_contents[current_section].push_back(0);
-      section_contents[current_section].push_back(14);
+      section_contents[current_section].push_back(4);//najnizi bajt
+      
+      
       section_contents[current_section].push_back(151);
+      section_contents[current_section].push_back(14);
+      section_contents[current_section].push_back(0);
+      section_contents[current_section].push_back(4);
+      
     }
     else if (instruction == "call"){
       //jedan operand, adresa funkcije koja se poziva
@@ -216,25 +218,32 @@ void Assembler::addLine(Line& line){
       //00100000 00000000 0000gornja4 donjih8D
       std::string operand = operands.front();
       //ovaj deo ispod mozemo da stavimo u funkciju parseJumpOperands()
-      parseJumpOperands(operand,line);
-      section_contents[current_section].push_back(0);
       section_contents[current_section].push_back(32);
+      section_contents[current_section].push_back(0);
+      parseJumpOperands(operand,line);
+      
+      
 
     }
     else if (instruction == "ret"){
       //nema operanada, radi se pop pc
       //pc<=mem[sp];sp<=sp+4
-      section_contents[current_section].push_back(4);//najnizi bajt
-      section_contents[current_section].push_back(0);
-      section_contents[current_section].push_back(254);
       section_contents[current_section].push_back(147);//najvisi bajt
+      section_contents[current_section].push_back(254);
+      section_contents[current_section].push_back(0);
+      section_contents[current_section].push_back(4);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "jmp"){
       //pc<=operand, 1 operand
       std::string operand = operands.front();
-      parseJumpOperands(operand,line);
-      section_contents[current_section].push_back(0);
       section_contents[current_section].push_back(48);//najvisi bajt
+      section_contents[current_section].push_back(0);
+      parseJumpOperands(operand,line);
+      
+      
     }
     else if (instruction == "beq"){
       //2 registra, operand koji predstavlja adresu za skok
@@ -245,12 +254,15 @@ void Assembler::addLine(Line& line){
 
       int reg_code1 = parseRegister(reg1);
       int reg_code2 = parseRegister(reg2);
-
-      parseJumpOperands(operand,line);
       reg_code1 = reg_code1<<4;
       reg_code1+=reg_code2;
-      section_contents[current_section].push_back(reg_code1);
+
       section_contents[current_section].push_back(49);
+      section_contents[current_section].push_back(reg_code1);
+      parseJumpOperands(operand,line);
+      
+      
+      
 
     }
     else if (instruction == "bne"){
@@ -260,12 +272,14 @@ void Assembler::addLine(Line& line){
 
       int reg_code1 = parseRegister(reg1);
       int reg_code2 = parseRegister(reg2);
-
-      parseJumpOperands(operand,line);
       reg_code1 = reg_code1<<4;
       reg_code1+=reg_code2;
-      section_contents[current_section].push_back(reg_code1);
       section_contents[current_section].push_back(50);
+      section_contents[current_section].push_back(reg_code1);
+      parseJumpOperands(operand,line);
+      
+      
+      
     }
     else if (instruction == "bgt"){
       std::string reg1 = operands[0];
@@ -274,12 +288,14 @@ void Assembler::addLine(Line& line){
 
       int reg_code1 = parseRegister(reg1);
       int reg_code2 = parseRegister(reg2);
-
-      parseJumpOperands(operand,line);
       reg_code1 = reg_code1<<4;
       reg_code1+=reg_code2;
-      section_contents[current_section].push_back(reg_code1);
       section_contents[current_section].push_back(51);
+      section_contents[current_section].push_back(reg_code1);
+      parseJumpOperands(operand,line);
+      
+      
+      
     }
     else if (instruction == "push"){
       //jedan operand koji je neki registar,
@@ -290,10 +306,13 @@ void Assembler::addLine(Line& line){
       int sp = 14;
       sp = sp<<4;
       sp+=reg_code;
-      section_contents[current_section].push_back(-4);//najnizi bajt
-      section_contents[current_section].push_back(0);
-      section_contents[current_section].push_back(sp);
       section_contents[current_section].push_back(129);//najvisi bajt
+      section_contents[current_section].push_back(sp);
+      section_contents[current_section].push_back(0);
+      section_contents[current_section].push_back(-4);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "pop"){
       //jedan operand-registar
@@ -303,49 +322,205 @@ void Assembler::addLine(Line& line){
       int sp = 14;
       sp = sp<<4;
       sp+=reg_code;
-      section_contents[current_section].push_back(4);//najnizi bajt
-      section_contents[current_section].push_back(0);
-      section_contents[current_section].push_back(sp);
       section_contents[current_section].push_back(147);//najvisi bajt
+      section_contents[current_section].push_back(sp);
+      section_contents[current_section].push_back(0);
+      section_contents[current_section].push_back(4);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "xchg"){
       //2 operanada registri oba,
       //temp = r1, r1 = r2, r2=temp -> zamena vrednosti 2 registra
+      //0100 0000 0000 BBBB CCCC 0000 0000
 
+      
+
+
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      r2_code<<4;
+      section_contents[current_section].push_back(64);//najvisi bajt
+      section_contents[current_section].push_back(r1_code);
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "add"){
       //2 registra
       //r1 = r1+r2
+      //0101 0000 AAAA BBBB CCCC 0000 0000 0000
+      //A->r2, B->r1, C->r2
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
+      section_contents[current_section].push_back(80);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "sub"){
       //2 registra
       //r1 = r1-r2
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
+      section_contents[current_section].push_back(81);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "mul"){
       //2 reg, r1 = r1*r2
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
+      section_contents[current_section].push_back(82);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "div"){
       //2 reg, r1 = r1/r2
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
+      section_contents[current_section].push_back(83);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "not"){
       //1 reg, r1 = !r1
+      // 0110 0000 AAAA BBBB CCCC 0000 0000 0000
+      // ovde je c 0, znaci zadnja 2 su 0
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      r2_code<<4;
+      r2_code+=r1_code;
+      section_contents[current_section].push_back(96);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      section_contents[current_section].push_back(0);
+      
+      
+      
     }
     else if (instruction == "and"){
       //2 reg, r1= r1&r2
+      //ovde c == A
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
+
+      
+      section_contents[current_section].push_back(97);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
+
     }
     else if (instruction == "or"){
       //2 reg , r1 = r1|r2
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
+
+      
+      section_contents[current_section].push_back(98);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "xor"){
       //2 reg, r1 = r1^r2
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
 
+      section_contents[current_section].push_back(99);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "shl"){
       //2 registra
+      //r2 = r2<r1
+      //0111 0000 AAAA BBBB CCCC 0000 0000 0000
+      //A->r2, B->r1, c->r2
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
+
+      section_contents[current_section].push_back(112);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
     else if (instruction == "shr"){
       //2 registra
+      int r1_code = parseRegister(operands[0]);
+      int r2_code = parseRegister(operands[1]);
+      int c_code = r2_code<<4;
+      r2_code<<4;
+      r2_code+=r1_code;
+
+      
+      section_contents[current_section].push_back(113);//najvisi bajt
+      section_contents[current_section].push_back(r2_code);
+      section_contents[current_section].push_back(c_code);
+      section_contents[current_section].push_back(0);//najnizi bajt
+      
+      
+      
     }
+    /*
+    Ovde treba da se napravi prvo funkcija za parsiranje operanada za rad
+    sa memorijom
+    */
     else if (instruction == "ld"){
       //operand + registar
     }
@@ -377,6 +552,20 @@ void Assembler::printTables(){
     "Global: "<<entry.second.is_global<<" - "<<
     "Section: "<<entry.second.section<<" - "<<std::endl;
   }
+  std::cout<<"section contents"<<std::endl;
+  /*
+  sve instrukcije po 4B
+  */
+  int count = 0;
+  for (auto& section:section_contents){
+    std::cout<<section.first<<std::endl;
+    for (auto& b:section.second){
+      if (count!=0 && count%32 == 0)std::cout<<std::endl;
+      std::cout<<std::bitset<8>(b)<<" ";
+      count+=8;
+    }
+    std::cout<<std::endl;
+  }
 }
 
 void Assembler::parseJumpOperands(std::string operand,Line line){
@@ -387,17 +576,20 @@ void Assembler::parseJumpOperands(std::string operand,Line line){
     int literal = stoi(operand);
     int bits_11_to_8 = (literal >> 8) & 0xF;
     int lowest_8 = literal & 0xFF;
-    
-    section_contents[current_section].push_back(lowest_8);
+
     section_contents[current_section].push_back(bits_11_to_8);
+    section_contents[current_section].push_back(lowest_8);
+    
     
   }
   else{
     //prvo proveravamo da li je simbol definisan
+    section_contents[current_section].push_back(0);
+    section_contents[current_section].push_back(0);
     if (symbol_table.count(operand) && symbol_table[operand].defined){
       //ako je global addend = 0 i value je vrednost simbola,
       //ako nije global value je sekcija i addend je 
-      int offset = location_counter-4;//4. bajt instrukcije
+      int offset = location_counter-1;//4. bajt instrukcije
       std::string sym;
       int addend;
 
@@ -423,7 +615,7 @@ void Assembler::parseJumpOperands(std::string operand,Line line){
           symbol_table[operand].flink = new ForwardRefsEntry();
           symbol_table[operand].flink->next = nullptr;
           symbol_table[operand].flink->section = current_section;
-          symbol_table[operand].flink->patch = location_counter-4;
+          symbol_table[operand].flink->patch = location_counter-1;
         }
         else{
           while(temp->next)temp=temp->next;
@@ -440,7 +632,7 @@ void Assembler::parseJumpOperands(std::string operand,Line line){
         symbol_table[operand].flink = new ForwardRefsEntry();
         symbol_table[operand].flink->next = nullptr;
         symbol_table[operand].flink->section = current_section;
-        symbol_table[operand].flink->patch = location_counter-4;
+        symbol_table[operand].flink->patch = location_counter-1;
       }
     }
   }
@@ -462,5 +654,5 @@ int Assembler::parseRegister(std::string register_name){
   if (register_name == "%r13") return 13;
   if (register_name == "%r14") return 14;
   if (register_name == "%r15") return 15;
-  
+  return -1;
 }
